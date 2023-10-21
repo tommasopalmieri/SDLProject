a1e3b53048f76f5981a88f0cbb96bcbdfecec8df
tommaso <tp@tommasopalmieri.com>
2023-10-21 12:02:06 +0200

    Pong CLone

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+
+/**
+* Author: Tommaso Palmieri
+* Assignment: Pong Clone
+* Date due: 2023-10-21, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 
@@ -33,7 +45,28 @@ const int VIEWPORT_X = 0,
 VIEWPORT_Y = 0,
 VIEWPORT_WIDTH = WINDOW_WIDTH,
 VIEWPORT_HEIGHT = WINDOW_HEIGHT;
+// Dimensions for paddle and ball
+const float PADDLE_WIDTH = 0.3f;
+const float PADDLE_HEIGHT = 1.5f;
+const float BALL_SIZE = 0.3f;
 
+// Ball speed
+const float BALL_SPEED = 0.03f;
+bool play_mode = true;
+bool play_dir = true;
+
+// Player 1 paddle
+glm::vec3 player1_position = glm::vec3(-4.5f, 0.0f, 0.0f);
+glm::vec3 player1_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+// Player 2 paddle
+glm::vec3 player2_position = glm::vec3(4.5f, 0.0f, 0.0f);
+glm::vec3 player2_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+
+// Ball
+glm::vec3 ball_position = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 ball_movement = glm::vec3(-BALL_SPEED, BALL_SPEED, 0.0f);
 float g_additional_shape_rotation = 0.0f;
 
 const char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
@@ -46,10 +79,15 @@ const int NUMBER_OF_TEXTURES = 1; // to be generated, that is
 const GLint LEVEL_OF_DETAIL = 0;  // base image level; Level n is the nth mipmap reduction image
 const GLint TEXTURE_BORDER = 0;   // this value MUST be zero
 
-const char PLAYER_SPRITE_FILEPATH[] = "C:\\Users\\tommaso\\Downloads\\soph.png";
-const char ADDITIONAL_SHAPE_TEXTURE_FILEPATH[] = "C:\\Users\\tommaso\\Downloads\\R.png"; 
+
+// move this file to where the executable is lauched. 
+const char PLAYER_SPRITE_FILEPATH[] = "soph.png";
+const char ADDITIONAL_SHAPE_TEXTURE_FILEPATH[] = "R.png"; 
 GLuint g_additional_shape_texture_id;
 
+
+glm::vec3 left_paddle_position = glm::vec3(-4.0f, 0.0f, 0.0f);
+glm::vec3 right_paddle_position = glm::vec3(4.0f, 0.0f, 0.0f);
 
 const float MIN_SIZE_CHANGE_INTERVAL = 1.0f; // 1 second, adjust as needed
 const float MAX_SIZE_CHANGE_INTERVAL = 3.0f; // 3 seconds, adjust as needed
@@ -132,7 +170,7 @@ void initialise()
 	// Open the first controller found. Returns null on error
 	g_player_one_controller = SDL_JoystickOpen(0);
 
-	g_display_window = SDL_CreateWindow("Hello, Textures!",
+	g_display_window = SDL_CreateWindow("Poor Coder's Pong",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		WINDOW_WIDTH, WINDOW_HEIGHT,
 		SDL_WINDOW_OPENGL);
@@ -169,96 +207,184 @@ void initialise()
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
-void process_input()
+bool checkCollision(const glm::vec3& a_pos, float awidth, float aheight,
+	const glm::vec3& b_pos, float bwidth, float bheight) {
+
+	return (a_pos.x - awidth / 2 < b_pos.x + bwidth / 2 &&
+		a_pos.x + awidth / 2 > b_pos.x - bwidth / 2 &&
+		a_pos.y - aheight / 2 < b_pos.y + bheight / 2 &&
+		a_pos.y + aheight / 2 > b_pos.y - bheight / 2);
+}
+
+void shutdown()
 {
-	g_player_movement = glm::vec3(0.0f);
+	SDL_JoystickClose(g_player_one_controller);
+	SDL_Quit();
+}
+
+void process_input() {
+	// Reset movements
+	player1_movement = glm::vec3(0.0f);
+	player2_movement = glm::vec3(0.0f);
 
 	SDL_Event event;
-
-	while (SDL_PollEvent(&event))
-	{
+	while (SDL_PollEvent(&event)) {
 		switch (event.type) {
 		case SDL_WINDOWEVENT_CLOSE:
 		case SDL_QUIT:
 			g_game_is_running = false;
 			break;
-
 		case SDL_KEYDOWN:
 			switch (event.key.keysym.sym) {
-			case SDLK_RIGHT:
-				g_player_movement.x = 1.0f;
-				break;
-			case SDLK_LEFT:
-				g_player_movement.x = -1.0f;
-				break;
 			case SDLK_q:
 				g_game_is_running = false;
 				break;
 			default:
 				break;
 			}
-		default:
 			break;
 		}
 	}
 
-	const Uint8* key_states = SDL_GetKeyboardState(NULL); // array of key states [0, 0, 1, 0, 0, ...]
+	const Uint8* key_states = SDL_GetKeyboardState(NULL);
 
-	if (key_states[SDL_SCANCODE_LEFT])
-	{
-		g_player_movement.x = -1.0f;
-	}
-	else if (key_states[SDL_SCANCODE_RIGHT])
-	{
-		g_player_movement.x = 1.0f;
+
+	if (key_states[SDL_SCANCODE_T]) {
+		play_mode = !play_mode;
 	}
 
-	if (key_states[SDL_SCANCODE_UP])
-	{
-		g_player_movement.y = 1.0f;
+	// Player 1 controls
+	if (key_states[SDL_SCANCODE_W]) {
+		player1_movement.y = 0.001f;
 	}
-	else if (key_states[SDL_SCANCODE_DOWN])
-	{
-		g_player_movement.y = -1.0f;
-	}
-
-	if (glm::length(g_player_movement) > 1.0f)
-	{
-		g_player_movement = glm::normalize(g_player_movement);
-	}
-}
-
-void update()
-{
-	float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND; // get the current number of ticks
-	float delta_time = ticks - m_previous_ticks; // the delta time is the difference from the last frame
-	m_previous_ticks = ticks;
-
-
-	g_additional_shape_rotation += DEGREES_PER_SECOND * delta_time;
-	if (g_additional_shape_rotation >= 360.0f) // reset the angle after one full rotation
-	{
-		g_additional_shape_rotation -= 360.0f;
+	else if (key_states[SDL_SCANCODE_S]) {
+		player1_movement.y = -0.001f;
 	}
 
-	shape_pos -= shape_mov * delta_time * 1.0f;
-
-	m_model_matrix = glm::mat4(1.0f);
-	m_model_matrix = glm::translate(m_model_matrix, shape_pos);
-
-	g_additional_shape_position -= shape_mov * delta_time * 1.0f;
+	if (play_mode) {
+		// Player 2 controls
+		if (key_states[SDL_SCANCODE_UP]) {
+			player2_movement.y = 0.001f;
+		}
+		else if (key_states[SDL_SCANCODE_DOWN]) {
+			player2_movement.y = -0.001f;
+		}
+	}
 
 }
+void update() {
 
-void draw_object(glm::mat4& object_model_matrix, GLuint& object_texture_id)
+
+	if (ball_position.x - BALL_SIZE <= -5.0f || ball_position.x + BALL_SIZE >= 5.0f) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Game Over", "This hopefully not too glitchy game is over...! ", g_display_window);
+		g_game_is_running = false; // Quit game when it's over
+		return;  // Exit from the update function
+	}
+
+
+
+	// Update ball position
+	ball_position += ball_movement * BALL_SPEED;
+
+	// Ball collision with top & bottom
+	if (ball_position.y+ BALL_SIZE*2 > 3.75 || ball_position.y-(BALL_SIZE*2) < -3.75) {
+		ball_movement.y = -ball_movement.y;
+	}
+
+
+
+	// Ball collision with paddles
+	if (checkCollision(ball_position, BALL_SIZE*2, BALL_SIZE*2, player1_position, PADDLE_WIDTH * 2, PADDLE_HEIGHT) ||
+		checkCollision(ball_position, BALL_SIZE*2, BALL_SIZE*2, player2_position, PADDLE_WIDTH * 2, PADDLE_HEIGHT)) {
+		ball_movement.x = -ball_movement.x;
+	}
+
+	// Update paddles
+	player1_position += player1_movement;
+
+	if (play_mode) {
+		player2_position += player2_movement;
+	}
+	else {
+		if (player2_position.y - (PADDLE_HEIGHT / 2) == -3.75) {
+			play_dir = false;
+		}
+		else if (player2_position.y + (PADDLE_HEIGHT / 2) == 3.75) {
+			play_dir = true;
+		}
+
+		if (play_dir) {
+			player2_position.y -= 0.01f;
+		}
+		else {
+			player2_position.y += 0.01f;
+		}
+	}
+
+	// Keep paddles within boundaries
+	if (player1_position.y + (PADDLE_HEIGHT/2) > 3.75) player1_position.y = 3.75 - (PADDLE_HEIGHT / 2);
+	if (player1_position.y - (PADDLE_HEIGHT / 2) < -3.75) player1_position.y = -3.75 + (PADDLE_HEIGHT / 2);
+	if (player2_position.y + (PADDLE_HEIGHT / 2) > 3.75) player2_position.y = 3.75 - (PADDLE_HEIGHT / 2);
+	if (player2_position.y - (PADDLE_HEIGHT / 2) < -3.75) player2_position.y = -3.75 + (PADDLE_HEIGHT / 2);
+}
+
+
+void draw_object(glm::mat4& object_model_matrix, GLuint object_texture_id = NULL)
 {
 	g_shader_program.set_model_matrix(object_model_matrix);
-	glBindTexture(GL_TEXTURE_2D, object_texture_id);
-	glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so we use 6 instead of 3
+
+	if (object_texture_id != NULL) {
+		glBindTexture(GL_TEXTURE_2D, object_texture_id);
+	
+	}
+
+	glDrawArrays(GL_TRIANGLES, 0, 6); 
 }
 
 void render() {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Vertices
+	glColor3f(0.0f, 0.0f, 0.0f);
+
+	// Vertices for the paddle
+	float paddle_vertices[] = {
+		-PADDLE_WIDTH / 2, -PADDLE_HEIGHT / 2,
+		PADDLE_WIDTH / 2, -PADDLE_HEIGHT / 2,
+		PADDLE_WIDTH / 2, PADDLE_HEIGHT / 2,
+		-PADDLE_WIDTH / 2, -PADDLE_HEIGHT / 2,
+		PADDLE_WIDTH / 2, PADDLE_HEIGHT / 2,
+		-PADDLE_WIDTH / 2, PADDLE_HEIGHT / 2
+	};
+
+	// Bind null for the texture to avoid using any texture
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// Render Player 1
+	glm::mat4 player1_model_matrix = glm::mat4(1.0f);
+	player1_model_matrix = glm::translate(player1_model_matrix, player1_position);
+	g_shader_program.set_model_matrix(player1_model_matrix);
+	glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, paddle_vertices);
+	glEnableVertexAttribArray(0);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	// Render Player 2
+	glm::mat4 player2_model_matrix = glm::mat4(1.0f);
+	player2_model_matrix = glm::translate(player2_model_matrix, player2_position);
+	g_shader_program.set_model_matrix(player2_model_matrix);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	// Set the color back to default
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	float ball_vertices[] = {
+		-BALL_SIZE / 2, -BALL_SIZE / 2,
+		BALL_SIZE / 2, -BALL_SIZE / 2,
+		BALL_SIZE / 2, BALL_SIZE / 2,
+		-BALL_SIZE / 2, -BALL_SIZE / 2,
+		BALL_SIZE / 2, BALL_SIZE / 2,
+		-BALL_SIZE / 2, BALL_SIZE / 2
+	};
 
 	// Vertices
 	float vertices[] = {
@@ -278,32 +404,19 @@ void render() {
 	glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, texture_coordinates);
 	glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
 
-	// Bind texture
-	draw_object(m_model_matrix, g_player_texture_id);
 
+	m_model_matrix = glm::mat4(1.0f);
+	m_model_matrix = glm::translate(m_model_matrix, ball_position);
+	draw_object(m_model_matrix, g_player_texture_id);  
 
-	additional_shape_model_matrix = glm::mat4(1.0f);
-	additional_shape_model_matrix = glm::translate(additional_shape_model_matrix, g_additional_shape_position);
-	additional_shape_model_matrix = glm::rotate(additional_shape_model_matrix, glm::radians(g_additional_shape_rotation), glm::vec3(0.0f, 0.0f, 1.0f));
-	draw_object(additional_shape_model_matrix, g_additional_shape_texture_id);
-
-
-	// We disable two attribute arrays now
 	glDisableVertexAttribArray(g_shader_program.get_position_attribute());
 	glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
 
 	SDL_GL_SwapWindow(g_display_window);
 }
 
-void shutdown()
-{
-	SDL_JoystickClose(g_player_one_controller);
-	SDL_Quit();
-}
 
-/**
- Start here—we can see the general structure of a game loop without worrying too much about the details yet.
- */
+
 int main(int argc, char* argv[])
 {
 	initialise();
